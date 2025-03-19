1745d948e983b9e8321be9a5e9a49b4d8db5c018
samir suroshe <sameersuroshe50@gmail.com>
2025-03-19 22:05:49 +0530

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,318 @@
+import expressImg from '../assets/express.png';
+import mysql from '../assets/mysql.png';
+import nodejs from '../assets/nodejs.png';
+import php from '../assets/php.png';
+import github from '../assets/github.png';
+import flutter from '../assets/flutter.png';
+import aws from '../assets/aws.png';
+import firebase from '../assets/firebase.png';
+import kotlin from '../assets/kotlin.png';
+import mongodb from '../assets/mongodb.png';
+import vercel from '../assets/vercel.png';
+import render from '../assets/render.png';
+import dart from '../assets/dart.png';
+import tailwind from '../assets/tailwind.png';
+import smartdwell from '../assets/smartdwell.png';
+import ignitech from '../assets/ignitech.png';
+import wellbeing from '../assets/wellbeing.png';
+import sos from '../assets/sos.png';
+import whatsapp from '../assets/whatsapp.png';
+
+export const Bio = {
+    name: "Samir Suroshe",
+    roles: [
+      "Mobile Developer",
+      "Full Stack Developer",
+      "MERN Developer",
+      "DevOps Engineer"
+    ],
+    description:
+      " Software Developer skilled in full-stack web and mobile app development using Flutter, React, and Node.js. Proficient in native Android development with Java and Kotlin, and experienced in deploying cloud-based applications on AWS. Passionate about solving real-world problems, writing clean code, and collaborating on impactful projects.",
+    github: "https://github.com/samirsuroshe18",
+    resume: "https://drive.google.com/file/d/1-CLSqsK0fE5eJKRaPGMUzzsMIjrtxV7f/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/pranay-sanap-0b09282b6/",
+    twitter: "https://x.com/pranay_sanap",
+    Mail: "mailto:pranaysanap10@gmail.com",
+  };
+  
+  export const skills = [
+    {
+      title: "Programming Languages",
+      skills: [
+        {
+          name: "JavaScript",
+          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Java",
+          image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "Kotlin",
+          image: kotlin,
+        },
+        {
+          name: "Dart",
+          image: dart,
+        },
+        {
+          name: "Python",
+          image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "PHP",
+          image: php,
+        },
+      ],
+    },
+    {
+      title: "Frontend Development",
+      skills: [
+        {
+          name: "React.js",
+          image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image: "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "Tailwind",
+          image: tailwind,
+        },
+        {
+          name: "Bootstrap",
+          image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+      ],
+    },
+    {
+      title: "Backend & Database",
+      skills: [
+        {
+          name: "Node.js",
+          image: nodejs,
+        },
+        {
+          name: "Express.js",
+          image: expressImg,
+        },
+        {
+          name: "MySQL",
+          image: mysql,
+        },
+        {
+          name: "MongoDB",
+          image: mongodb,
+        },
+      ],
+    },
+    {
+      title: "Mobile App Development",
+      skills: [
+        {
+          name: "Flutter",
+          image: flutter,
+        },
+        {
+          name: "XML",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+        },
+        {
+          name: "Android Studio",
+          image: "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      ],
+    },
+    {
+      title: "Development & Deployment Tools",
+      skills: [
+        {
+          name: "Git",
+          image: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+        },
+        {
+          name: "GitHub",
+          image: github,
+        },
+        {
+          name: "Postman",
+          image: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+        },
+        {
+          name: "VS Code",
+          image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+      ],
+    },
+    {
+      title: "Cloud & Hosting",
+      skills: [
+        {
+          name: "AWS",
+          image: aws,
+        },
+        {
+          name: "Firebase",
+          image: firebase,
+        },
+        {
+          name: "Netlify",
+          image: "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "Vercel",
+          image: vercel,
+        },
+        {
+          name: "Render",
+          image: render,
+        },
+      ],
+    },
+  ];
+  
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: smartdwell,
+      role: "Software Developer",
+      company: "Smartdwell Technologies",
+      date: "May 2024 - Present",
+      desc: "I was responsible for creating software solutions that aligned with the business needs. Developed a visitor management app that now supports 100+ daily users, along with a water level indication app built with Flutter. Built and deployed 3+ React-based apps on AWS ec2, helping streamline internal processes. Contributed to shaping the company’s software capabilities, improving both operational efficiency and the user experience.",
+      // doc: "certificate link",
+    },
+    {
+      id: 1,
+      img: ignitech,
+      role: "Android Developer",
+      company: "Ignitech",
+      date: "June 2023 - July 2023",
+      desc: "Designed and implemented user-friendly interfaces using XML and Java, improving overall user experience and accessibility. Developed and maintained app features using Android Studio, Java, XML, SQLite, and sensors, ensuring seamless functionality and performance.",
+      doc: "https://drive.google.com/file/d/1TyqB2LAeX_m2yAI2091QmU-YXxbv4Uwp/view?usp=sharing"
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 1,
+      img: "https://gpthane.org.in/wp-content/uploads/2023/07/logo.png",
+      school: "Government Polytechnic, Thane",
+      date: "Aug 2021 - Aug 2024",
+      grade: "91.43%",
+      desc: "I completed a Diploma in Computer Engineering at Government Polytechnic, Thane, with an average percentage of 91.43%. My coursework included Data Structures, Algorithms, and DBMS, along with proficiency in programming languages like C, C++, Java, Python, and Android Studio, and a solid grasp of computer system fundamentals.",
+      degree: "Diploma in Computer Engineering",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Wellbeing",
+      date: "Jan 2024 - Feb 2024",
+      description: "The Wellbeing App is an Android application designed to promote mental, creative and emotional wellbeing by encouraging users, particularly teenagers and people, to engage in positive creative activities and tasks. It was created to address the growing need for tools that support mental health and foster a sense of community and keep creative and positive. The app allows users to create tasks related to good deeds or creative, earn points (wellpoints) for completing tasks, and connect with others who share similar interests in promoting wellbeing. The project is important to us as it aims to make a positive impact on the lives of individuals and communities. It was created in Feb 2024 and is available for download on Github. Application can be found here. To use the app, simply download .apk file from the github and install on your android device, create an account, and start completing tasks to earn wellpoints.",
+      image: wellbeing,
+      tags: [
+        "Android Studio",
+        "Java",
+        "XML",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "Cloudinary",
+        "Volley Library",
+        "Postman",
+        "Figma",
+      ],
+      category: "android app",
+      live: "https://github.com/samirsuroshe18/WellBeing-App/releases/download/1.0.0/wellbeing-v1.0.apk",
+      github: "https://github.com/samirsuroshe18/WellBeing-app---Android-studio",
+    },  
+    {
+      id: 1,
+      title: "SOS Emergency App",
+      date: "Nov 2023 - Dec 2024",
+      description: "Our SOS application is designed to provide quick assistance during emergencies. It allows users to add and manage emergency contacts, customize emergency messages, access helpline numbers, and receive first aid information. The highlight of the app is the Emergency Mode feature, which, when activated, sends an SOS message along with the user's current location to registered emergency contacts. It was created in Dec 2023 and is available for download on Github. Application can be found here. To use the app, simply download it from the github, and use it.",
+      image: sos,
+      tags: [
+        "Android Studio",
+        "Java",
+        "XML",
+        "Figma",
+        "Location",
+      ],
+      category: "android app",
+      live: "https://github.com/samirsuroshe18/SOS-Emergency-App/releases/download/1.0.0/app-debug.apk",
+      github: "https://github.com/samirsuroshe18/SOS-Emergency-App",
+    },  
+    {
+      id: 2,
+      title: "Chat App",
+      date: "June 2023 - Aug 2023",
+      description: "My ChatApp is a messaging application for Android devices, designed to provide users with a seamless chatting experience similar to popular messaging apps like WhatsApp. It allows users to exchange messages individually or in group chats, register and log in using their Google accounts, and manage their profiles. It was created in July 2023 and is available for download on Github. Application can be found here. To use the app, simply download .apk file from the github and install on your android device, create an account, and start chatting.",
+      image: whatsapp,
+      tags: [
+        "Android Studio",
+        "Java",
+        "XML",
+        "Google Auth",
+        "Firebase",
+        "Erasor.io",
+        "Figma",
+      ],
+      category: "android app",
+      live: "https://github.com/samirsuroshe18/My-ChatApp/releases/download/1.0.0/app-debug.apk",
+      github: "https://github.com/samirsuroshe18/My-ChatApp",
+    },  
+    {
+      id: 3,
+      title: "InflluenceIQ ",
+      date: "Dec 2024 - Jan 2025",
+      description:
+        "InfluenceIQ is an AI-driven social media analytics platform designed to provide deep insights into social media engagement and content strategies. By leveraging Generative AI (GenAI) technologies, the platform processes user account data and answers insightful questions about content performance, audience engagement, and strategic improvements.",
+      image:
+        "imagelink",
+      tags: [
+        "React Js",
+        "Langflow",
+        "Node Js",
+        "Gen AI",
+        "Express Js",
+        "Data stax",
+        "Redux",
+      ],
+      category: "web app",
+      github: "github",
+      webapp: "link",
+    },
+    {
+      id: 4,
+      title: "Weather App ",
+      date: "Jan 2024 ",
+      description:
+        "Stay ahead of the forecast with our cutting-edge React JS weather app, powered by the OpenWeather API. Effortlessly track current weather conditions, hourly forecasts, and extended outlooks with intuitive navigation and sleek design. With real-time updates and accurate data, our app ensures you're always prepared for whatever Mother Nature throws your way. Whether you're planning outdoor activities or simply staying informed, our React JS weather app has you covered, rain or shine.",
+      image:
+        "",
+      tags: ["React JS", "OpenWeather API", "Axios",],
+      category: "web app",
+      github: "",
+      webapp: "",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2021, text: "Started my journey" },
+    { year: 2023, text: "First Internship" },
+    { year: 2024, text: "Started learning IOT" },
+    { year: 2024, text: "Shared my projects with the world" },
+    { year: 2025, text: "Developing Web/Desktop Applications" },
+  ];
